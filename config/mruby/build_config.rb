3b64791bed0c380eac5046b747a551bebe83e688
sawanoboly <sawanoboriyu@higanworks.com>
2020-09-06 03:31:38 +0900

    remove supported libs

--- config/mruby/build_config.rb
+++ config/mruby/build_config.rb
@@ -7,13 +7,11 @@ MRuby::Build.new do |conf|
   #
   # Recommended for ngx_mruby
   #
-  conf.gem :github => 'iij/mruby-io'
   conf.gem :github => 'iij/mruby-env'
   conf.gem :github => 'iij/mruby-dir'
   conf.gem :github => 'iij/mruby-digest'
   conf.gem :github => 'iij/mruby-process'
   conf.gem :github => 'iij/mruby-pack'
-  conf.gem :github => 'iij/mruby-socket'
   conf.gem :github => 'mattn/mruby-json'
   conf.gem :github => 'mattn/mruby-onig-regexp'
   conf.gem :github => 'matsumoto-r/mruby-redis'
@@ -24,7 +22,10 @@ MRuby::Build.new do |conf|
   conf.gem :github => 'matsumoto-r/mruby-mutex'
   conf.gem :github => 'matsumoto-r/mruby-localmemcache'
   conf.gem :github => 'matsumoto-r/mruby-httprequest'
-  conf.gem :github => 'kaihar4/mruby-ipaddress_matcher'
+
+  # conf.gem :github => 'iij/mruby-io'
+  # conf.gem :github => 'iij/mruby-socket'
+  # conf.gem :github => 'kaihar4/mruby-ipaddress_matcher'
 
   # ngx_mruby extended class
   conf.gem './mrbgems/ngx_mruby_mrblib'
